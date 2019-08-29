81d456401d6903ea4a898d2567979fba2b333c46
Svijay98 <40879687+Svijay98@users.noreply.github.com>
2019-08-29 17:55:43 +0530

    Made changes for general scale
    copy pasted code from other shiv file and made necessary changes , such as selectUniform function

--- shivwithuniformpts.m
+++ shivwithuniformpts.m
@@ -6,19 +6,22 @@ image = imresize(gray,[500 500]);
 
 xy = zeros(1,2);
 i =1;
-for row = 1:5:500
-    for col = 1:5:500
-        points = detectMinEigenFeatures(image,'ROI',[row,col,5,5]);
-        strongest = selectUniform(points,10,[5 5]);
-        count=strongest.Count
-        if count>0
-            xy(i,1) = uint32(strongest.Location(1));
-            xy(i,2) = uint32(strongest.Location(2));
-            i = i+1;
-            %count = count - 1;
+for j = 1:2
+    k =uint32(5*((1.414)^(j-1)));
+    for row = 1:k:(501-k)
+        for col = 1:k:(501-k)
+            points = detectMinEigenFeatures(image,'ROI',[row,col,k,k]);
+            strongest = selectUniform(points,5,[floor(5*((1.414)^(j-1))),floor(5*((1.414)^(j-1)))]);
+            count=strongest.Count;
+            if count>0
+                xy(i,1) = uint32(strongest.Location(1));
+                xy(i,2) = uint32(strongest.Location(2));
+                i = i+1;
+                %count = count - 1;
+            end
         end
     end
-end
+end  
 points = cornerPoints(xy);
 marker = insertMarker(image,points,'circle','size',5 );
 imshow(marker)
