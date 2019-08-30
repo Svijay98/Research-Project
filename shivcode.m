ad2df425bbec51813ce135c5f069576ce0e3cf95
Svijay98 <40879687+Svijay98@users.noreply.github.com>
2019-08-30 17:25:05 +0530

    Proper Dense Sampling procedure
    Rather  than taking different window sizes, taking properly scaled images, 8 spatial scales to be exact was done

--- shivcode.m
+++ shivcode.m
@@ -6,10 +6,13 @@ image = imresize(gray,[500 500]);
 
 xy = zeros(1,2);
 i =1;
-
+for j=1:9
     k =5;
-    for row = 1:k:6
-        for col = 1:k:6
+    m = uint32((501)*(0.707^(j-1)));
+    n = rem(m,5);
+    m = m-n+1;
+    for row = 1:k:m-k
+        for col = 1:k:m-k
             points = detectMinEigenFeatures(image,'ROI',[row,col,k,k]);
             strongest = selectStrongest(points,1);
             count=strongest.Count;
@@ -22,11 +25,11 @@ i =1;
             end
         end
     end
+end
     
 points = cornerPoints(xy);
-marker = insertMarker(image(1:7,1:7),points,'circle','size',1);
+marker = insertMarker(image,points,'plus','size',2 );
 imshow(marker)
-truesize([400 500])
 
 
 %points = detectMinEigenFeatures(xy);
