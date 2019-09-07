4ea24ba76a5f19b1a008d4b53671afcf4f3e7ec2
Shivmohith <shivmohith8@gmail.com>
2019-09-07 22:45:28 +0530

    Add files via upload

--- Week3/KLT_trajectory.m
+++ Week3/KLT_trajectory.m
@@ -1,17 +1,23 @@
 clc;
-%v = VideoReader('D:\Research Project\Matlab\KTH_dataset\handwaving\person01_handwaving_d1_uncomp.avi');
-v = VideoReader('traffic.mp4');
-t = 1./v.FrameRate
-frame1 = readFrame(v);
-prev_points = detectMinEigenFeatures(rgb2gray(frame1));
-tracker = vision.PointTracker('MaxBidirectionalError',1);
-initialize(tracker,prev_points.Location,rgb2gray(frame1));
-i = 3;
+v = VideoReader('D:\Research Project\Matlab\KTH_dataset\handwaving\person01_handwaving_d1_uncomp.avi');
+%v = VideoReader('D:\Research Project\Matlab\KTH_dataset\running\person02_running_d1_uncomp.avi');
+%v = VideoReader('traffic.mp4');
+prev_frame = readFrame(v);
+prev_frame = rgb2gray(prev_frame);
+prev_frame = imresize(prev_frame, [256 256]);
+prev_features = detectMinEigenFeatures(prev_frame);
+tracker = vision.PointTracker();
+initialize(tracker,prev_features.Location,prev_frame);
+prev_points = prev_features.Location;
 while hasFrame(v)
-    frame = readFrame(v);
-    frame = rgb2gray(frame);
-    [curr_points,validity] = tracker(frame);
-    out = insertMarker(frame,points,'+');
-    imshow(out);
+    curr_frame = readFrame(v);
+    curr_frame = rgb2gray(curr_frame);
+    curr_frame = imresize(curr_frame, [256 256]);
+    [curr_points,validity] = tracker(curr_frame);
+    ax = axes;
+    showMatchedFeatures(prev_frame,curr_frame,prev_points,curr_points,'PlotOptions',{'.','.','y-'});
+    prev_points = curr_points;
+    prev_frame = curr_frame;
     i = i - 1;
 end
+
