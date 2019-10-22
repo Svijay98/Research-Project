4063defc50a35fc29a7bce06fbc1d207b736100b
Sanjanav-98 <40431641+Sanjanav-98@users.noreply.github.com>
2019-10-22 21:58:52 +0530

    Update testcslbp.m

--- week7/testcslbp.m
+++ week7/testcslbp.m
@@ -1,29 +1,20 @@
-v = VideoReader('visiontraffic.avi');
-cslbp=zeros(1,1);
- for i=1:15
-while hasFrame(v)
-    frameRGB = readFrame(v);
-    frameGray = rgb2gray(frameRGB);
-    t = CSLBP(frameGray);
-end
-    cslbp=cslbp+t;
- end
- 
-function lbp = CSLBP(I)
+function lbp = testcslbp(cuboid)
     lbp=zeros(1024,1);
-    [x y]=size(I);
-    for i=2:x-1
-        for j=2:y-1
-            a = (((I(i,j) - I(i, j+1)) ) * 2^0 );  
-            b = (((I(i,j) - I(i+1, j+1)) ) * 2^1 ); 
-            c = (((I(i,j) - I(i+1, j)) ) * 2^2 ); 
-            d = (((I(i,j) - I(i+1, j-1))  ) * 2^3 );
-            e = (((I(i,j) - I(i, j-1))  ) * 2^4 );
-            f = (((I(i,j) - I(i-1, j-1))  ) * 2^5 );
-            g = (((I(i,j) - I(i-1, j))  ) * 2^6 ); 
-            h = (((I(i,j) - I(i-1, j+1))  ) * 2^7 ); 
-            k=a+b+c+d+e+f+g+h;
-            lbp(k+1) = lbp(k+1) + 1;
-        end
-    end 
+    for t=1:5
+        [x y]=size(cuboid);
+        for i=2:x-1
+            for j=2:y-1
+                a = (((cuboid(i,j) - cuboid(i, j+1)) ) * 2^0 );  
+                b = (((cuboid(i,j) - cuboid(i+1, j+1)) ) * 2^1 ); 
+                c = (((cuboid(i,j) - cuboid(i+1, j)) ) * 2^2 ); 
+                d = (((cuboid(i,j) - cuboid(i+1, j-1))  ) * 2^3 );
+                e = (((cuboid(i,j) - cuboid(i, j-1))  ) * 2^4 );
+                f = (((cuboid(i,j) - cuboid(i-1, j-1))  ) * 2^5 );
+                g = (((cuboid(i,j) - cuboid(i-1, j))  ) * 2^6 ); 
+                h = (((cuboid(i,j) - cuboid(i-1, j+1))  ) * 2^7 ); 
+                k=a+b+c+d+e+f+g+h;
+                lbp(k+1) = lbp(k+1) + 1;
+            end
+        end 
+    end
 end
